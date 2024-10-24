3ab07c8530221fcc90da65f1455d70b1be43532e
abdus quadri <asquadri1@gmail.com>
2024-10-24 13:58:25 -0400

    updated script to ask for user input to continue

--- platform-enable-disable-agentless/cwp-bulk-purge.py
+++ platform-enable-disable-agentless/cwp-bulk-purge.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 import argparse
 import requests
 import json
@@ -8,56 +7,67 @@ import os
 import pprint
 import sys
 
-
 current_dir = os.path.dirname(__file__)
 sys.path.append(os.path.abspath(os.path.join(current_dir, '..', 'utilities')))
 from pc_auth import *
 from pc_cache import *
 from pc_api import *
 
-
 pp = pprint.PrettyPrinter(indent=5)
-
 
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-v", "--verbose", action='store_true', help="Print Verbose Messages")
 argParser.add_argument("-f", "--file", default="purge_accounts.json", help="Define Cache File")
 argParser.add_argument("-c", "--cache", action='store_true', help="Cache Results")
-argParser.add_argument("-x", "--config", action='store', help="Authorization - Config File (~/.prismacloud)",required=True)
+argParser.add_argument("-x", "--config", action='store', help="Authorization - Config File (~/.prismacloud)", required=True)
+# added this instead to read the config file from the same directory
+#argParser.add_argument("-x", "--config", action='store', help="Authorization - Config File (/.prismacloud)", required=True)
 
 args = argParser.parse_args()
 
-config_file = os.path.join(os.path.expanduser('~/.prismacloud'),args.config)
-if(args.verbose is True):print("Auth Config File >",config_file)
+config_file = os.path.join(os.path.expanduser('~/.prismacloud'), args.config)
+if(args.verbose is True): print("Auth Config File >", config_file)
 pc = json.loads(connect(config_file))
 
-
-#Looping Defaults
+# Looping Defaults
 items = []
 page_size = 50
 
 if args.cache:
-    items = create_cache(pc,args,"/api/v1/cloud-scan-rules")
+    items = create_cache(pc, args, "/api/v1/cloud-scan-rules")
 
-
-#If Cache, then use file to query
+# Read from cache file
 print("Read from cache file", args.file)
 with open(args.file, 'r') as file:
     items = json.load(file)
-if(args.verbose is True):print("Total Records in Cache: ",len(items))   
+if(args.verbose is True): print("Total Records in Cache: ", len(items))
 
-action_items = []
+def print_accounts_for_confirmation(accounts):
+    """Prints the accounts to be deleted and prepares them for user confirmation."""
+    account_ids = [account['credential']['accountID'] for account in accounts]
+    print("The following accounts are marked as deleted and will be removed:")
+    for account_id in account_ids:
+        print(account_id)
+    return account_ids
 
-for index, i in enumerate(items):
-    account = items[index]["deleted"]
-    if account is True:
-        print("Removing Account: ",i["credential"]["accountName"])   
-        action_items.append(i)
-        pc_request(auth=pc,method="delete",url=pc["twistlockUrl"]+"/api/v1/cloud-scan-rules/"+i["credential"]["accountID"],platform=False,verbose=False)
+def get_user_confirmation():
+    """Gets user confirmation to proceed with deletion."""
+    confirmation = input("Do you want to proceed with deleting these accounts? (yes/no): ")
+    return confirmation.lower() == 'yes'
 
-if(args.verbose is True):print("Total Accounts Deleted",len(action_items))
+# Filter out the deleted accounts
+action_items = [item for item in items if item.get('deleted') is True]
 
+# Print accounts for user confirmation
+account_ids_to_delete = print_accounts_for_confirmation(action_items)
 
-
-# with open("action_items.json", 'w') as file:
-#     json.dump(action_items, file, indent=3)
+# Ask for user confirmation before deletion
+if get_user_confirmation():
+    # Perform deletion of accounts
+    for account in action_items:
+        account_id = account["credential"]["accountID"]
+        print(f"Removing Account: {account['credential']['accountName']}")
+        pc_request(auth=pc, method="delete", url=pc["twistlockUrl"] + f"/api/v1/cloud-scan-rules/{account_id}", platform=False, verbose=args.verbose)
+    if(args.verbose is True): print("Total Accounts Deleted", len(action_items))
+else:
+    print("Operation canceled by user.")
